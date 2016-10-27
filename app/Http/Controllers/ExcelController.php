e1c3597782c23b791f4ae35833eda35220587c3d
inGood <Ade@AdeMBP.local>
2016-10-27 21:52:17 +0800

    test laravel-excel

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -29,24 +29,49 @@ class ExcelController extends Controller
         // iconv在转换字符"—"到gb2312时会出错,可以采用 `//IGNORE` 忽略转换中的错误。//TRANSLIT
 
         Excel::create('haha',function ($excel) use ($cellData){
-            $excel->sheet('score', function ($sheet) use ($cellData){
+            $excel->sheet('学生成绩', function ($sheet) use ($cellData){
                 $sheet->rows($cellData);
-            })->store('xls');
+            })->export('xls');
         });
     }
 
     public function import()
     {
 
-        $filePath = 'storage/exports/'.'haha.xls';
+        $filePath = 'storage/exports/'.'txl.xls';
         $data = [];
         // &$data 加上 `&` 则变量为地址传递,对象外将可以调用改版的内容。
         Excel::load($filePath, function ($reader) use (&$data) {
-//            $data = $reader->get()->toArray();
-            $data = $reader->getSheet(0)->toArray();
+//            $data = $reader->all()->groupBy('department');
+//            $data=$reader->take(3)->all()->toJson();
+//            $data=$reader->limit(3)->toObject()->where('name','陈斌');
+//            $data = $reader->getSheet(0)->toArray();
 //            $data=$row->firstname;
+//            $data = $reader->dump(); // 输出读取结果,以对象的模式
+
+//            // Loop through all sheets
+//            $reader->each(function($row) {
+//                dump($row);
+//            });
+
         });
-        dd($data);
+
+        // 选择一个指定的表
+        Excel::selectSheets('abc')->load($filePath, function ($reader) use (&$data) {
+//            $data = $reader->dump(); // 输出读取结果,以对象的模式
+        });
+
+        // 通过索引选择一个指定的表
+        Excel::selectSheetsByIndex(0)->load($filePath, function ($reader) use (&$data) {
+//            $data = $reader->dump(); // 输出读取结果,以对象的模式
+        });
+
+        // 选择表中的列
+        Excel::selectSheetsByIndex(0)->load($filePath, function ($reader) use (&$data) {
+//            $data = $reader->dump(); // 输出读取结果,以对象的模式
+            $data = $reader->get(array('name','department'));
+            dd($data);
+        });
     }
 
 }
